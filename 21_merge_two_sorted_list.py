d9f4b32beeabaa24ba55cf6ef44b09669b131b03
hao134 <40970293+hao134@users.noreply.github.com>
2021-06-30 16:17:49 +0800

    linked list, recursion

---
+++ 21_merge_two_sorted_list.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# 1 iterative solution
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1: return l2
+        if not l2: return l1
+
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        if l2.val < l1.val:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+
+# 2 recursion solution
+class Solution2:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dum = ListNode(None)
+        prev = dum
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+
+        if not l1:
+            prev.next = l2
+        if not l2:
+            prev.next = l1
+
+        return dum.next
