5130d6c140395bac9d8f25ac68aa3f869d8bde37
hao134 <hao134@github.com>
2022-06-24 20:58:28 +0800

    two sum js and javascript

---
+++ 1_TwoSum.js
@@ -0,0 +1,26 @@
+const findTwoSum = function (nums, target) {
+  for (let p1 = 0; p1 < nums.length; p1++) {
+    const numberToFind = target - nums[p1];
+    for (p2 = p1 + 1; p2 < nums.length; p2++) {
+      if (numberToFind === nums[p2]) {
+        return [p1, p2];
+      }
+    }
+  }
+  return null; // no solution
+};
+
+// better way
+const findTwoSum2 = function (nums, target) {
+  const numsMap = {};
+  for (let p = 0; p < nums.length; p++) {
+    const currentMapVal = numsMap[nums[p]];
+    if (currentMapVal >= 0) {
+      return [currentMapVal, p];
+    } else {
+      const numberToFind = target - nums[p];
+      numsMap[numberToFind] = p;
+    }
+  }
+  return null;
+};
