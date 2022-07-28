9c72d4f58a3a96aa3ed778ac5922921650c72f01
hao134 <hao134@github.com>
2022-07-28 22:19:16 +0800

    92 medium

---
+++ 92_reverse_linkedList_between.js
@@ -0,0 +1,36 @@
+// consider example:
+// 1 -> 2 -> 3 -> 4 -> 5 -> null
+// left = 2 , right = 4
+
+const reverseBetween = function (head, left, right) {
+  let currentPos = 1,
+    currentNode = head,
+    start = head;
+
+  while (currentPos < left) {
+    start = currentNode;
+    currentNode = currentNode.next;
+    currentPos++;
+  }
+  // at this instance, start at 1, currentNode, currentPos at 2
+
+  let newList = null,
+    tail = currentNode;
+
+  while (currentPos >= left && currentPos <= n) {
+    const next = currentNode.next;
+    currentNode.next = newList;
+    newList = currentNode;
+    currentNode = next;
+    currentPos++;
+  }
+
+  start.next = newList;
+  tail.next = currentNode;
+
+  if (left > 1) {
+    return head;
+  } else {
+    return newList;
+  }
+};
