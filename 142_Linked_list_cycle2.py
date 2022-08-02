ed7e5103d6e6b1b358f09135798859b3cd74c4f5
hao134 <hao134@github.com>
2022-08-02 22:39:49 +0800

    add Floyd's solution

--- 142_Linked_list_cycle2.py
+++ 142_Linked_list_cycle2.py
@@ -1,3 +1,4 @@
+from sys import ps1
 from typing import Optional
 class ListNode:
     def __init__(self, x):
@@ -15,3 +16,32 @@ class Solution:
             head = head.next
         
         return None
+#Time:O(N)
+#Space:O(N)
+
+
+###### Floyd's solution:
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head: return None
+        slow = head
+        fast = head
+        while True:
+            slow = slow.next
+            fast = fast.next
+            if fast == None or fast.next == None:
+                return None
+            else:
+                fast = fast.next
+            if fast == slow:
+                break
+        
+        p1 = head
+        p2 = fast
+        while p1 != p2:
+            p1 = p1.next
+            p2 = p2.next
+        return p1 
+
+#Time:O(N)
+#Space:O(1)
